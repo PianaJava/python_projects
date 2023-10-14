1908b2f1f129d2636d699e01f9f3c021f074efb3
Piana <claudio.piana@capgemini.com>
2023-10-14 22:33:44 +0200

    02 code implemented

--- 02_number_guessing/number_guessing.py
+++ 02_number_guessing/number_guessing.py
@@ -1,19 +1,22 @@
 from random import randint
 
 # Start the program with basic setup
-lower_num, upper_num = 1, 10
-random_number: int = randint(lower_num, upper_num)
-print(f'Guess the number in the range from {lower_num} to {upper_num}.')
+min, max = 1, 10
+random_number: int = randint(min, max)
+print(f'Guess the number in the range from {min} to {max}.')
 
-# Run an infinite loop for the game
-while True:
+x=1
+max_attempts = 3
+
+# Run an infinite loop for the game till 3 guess
+while x <= max_attempts:
     # Get the user guess
     try:
         user_guess: int = int(input('Guess: '))
     except ValueError as e:
         print('Please enter a valid number.')
         continue
-
+    
     # Check the user guess
     if user_guess > random_number:
         print('The number is lower')
@@ -22,3 +25,8 @@ while True:
     else:
         print('You guessed it!')
         break
+    x += 1
+
+# If the loop exits without a correct guess
+if x > max_attempts:
+    print(f"GAME OVER! The correct number was {random_number}.")
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Those are taken from https://github.com/federicoazzu/python_projects
 
 #01 
 Importing some key words to complete the story.
-Usuful for multiple line code.
 
-
+#02 
+Guess number tool, maximum attemps can be changed, Game over message implemented
 
