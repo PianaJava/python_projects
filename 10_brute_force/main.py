94a2d1c1b3a4f3022369e8df43e2eb7e49dd88b6
Claudio Piana <pianaclaudio91@gmail.com>
2023-11-05 22:22:50 +0100

    10_ Added a prompt input for digits and symbols

--- 10_brute_force/main.py
+++ 10_brute_force/main.py
@@ -1,4 +1,4 @@
-import itertools
+import itertools             
 import string
 import time
 
@@ -15,7 +15,7 @@ def common_guess(word: str) -> str | None:
 
 
 # Goes through every combination of chars
-def brute_force(word: str, length: int, digits=False, symbols=False) -> str | None:
+def brute_force(word: str, length: int, digits: bool, symbols: bool) -> str | None:
     """Performs brute force on finding a word"""
 
     # Modify this for total symbols
@@ -28,7 +28,7 @@ def brute_force(word: str, length: int, digits=False, symbols=False) -> str | No
         chars += string.punctuation
 
     attempts: int = 0
-    for guess in itertools.product(chars, repeat=length):
+    for guess in itertools.product(chars, repeat=length): # the function generates all possible combinations of characters from the chars iterable with a specified length length
         attempts += 1
         guess: str = ''.join(guess)
 
@@ -37,28 +37,39 @@ def brute_force(word: str, length: int, digits=False, symbols=False) -> str | No
         # print(guess, attempts) # Comment this out when you're not testing
 
 
+def input_val(type_input: string) -> bool:
+    retry: bool = True
+    while retry:
+        input_value: str = input(f"Do you want to use {type_input}? Y/N: ")
+        if input_value.lower() == "y":
+            use_it: bool = True
+            retry = False
+        elif input_value.lower() == "n":
+            use_it: bool = False
+            retry = False
+    return use_it
+
+
 def main():
     print('Searching...')
-    password: str = 'pass1'
-
-    # Get the start time
-    start_time: float = time.perf_counter()
+    password: str = 't91#'
 
     # Search for common words before using the actual brute force
-    if common_match := common_guess(password):
+    if common_match := common_guess(password): #"walrus operator" (:=) in your code to assign the result of the brute_force function to the cracked variable while also checking if the result is truthy. 
         print(common_match)
     else:
-        if cracked := brute_force(password, length=5, digits=True, symbols=False):
-            print(cracked)
-        else:
-            print('There was no match...')
-
-    # Get the end time
-    end_time: float = time.perf_counter()
-
-    # Display the time it took
-    print(round(end_time - start_time, 2), 's')
-
+        include_digits = input_val("digits") 
+        include_symbols = input_val("symbols")
+        start_time: float = time.perf_counter()
+        for i in range(3,5):
+            if cracked := brute_force(password, length=i, digits=include_digits, symbols=include_symbols):
+                print(cracked)
+                break
+            else:
+                print('There was no match...')
+        end_time: float = time.perf_counter()
+        # Display the time it took
+        print("Time for the research: ", round(end_time - start_time, 2), 's')
 
 if __name__ == '__main__':
     main()
